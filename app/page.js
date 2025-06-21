fde42d5f2d9127e96356a72a990d7d366bed6e42
AtharvaGGourshete <goursheteatharva@gmail.com>
2025-06-21 12:34:45 +0530

    Header Icon accomplished

--- app/page.js
+++ app/page.js
@@ -3,10 +3,6 @@ import ContactUsPage from "./ContactUs/page";
 import HomePage from "./HomePage/page";
 import ServicesPage from "./ServicesPage/page";
 
-export const metadata = {
-  title: "Dakhya Financial Consultancy",
-  description: "",
-};  
 
 export default function Home() {
   return (
